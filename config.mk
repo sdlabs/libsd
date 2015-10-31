af4833fe83949a35b372189ab5a23f67579da438
Bobby Powers <bobbypowers@gmail.com>
2015-10-31 17:23:45 -0400

    build: cleanup indentation in config.mk

--- config.mk
+++ config.mk
@@ -12,30 +12,30 @@ INCS := -Ilibutf -Iexpat/lib
 
 #EXTENSION = '.exe'
 
-OPT := -O1
-#COVFLAGS := -ftest-coverage -fprofile-arcs
+OPT       = -O1
+#COVFLAGS = -ftest-coverage -fprofile-arcs
 
-#STATIC := -static
+#STATIC   = -static
 
-CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=600
-CFLAGS += $(STATIC) -g -std=c11 $(OPT) -pthread $(WARNFLAGS) $(INCS) $(CPPFLAGS)
-CFLAGS += -fPIC
-#CFLAGS += -flto
-#CFLAGS += -Wunsafe-loop-optimizations
-CFLAGS += $(COVFLAGS)
+CPPFLAGS  = -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=600
+CFLAGS   += $(STATIC) -g -std=c11 $(OPT) -pthread $(WARNFLAGS) $(INCS) $(CPPFLAGS)
+CFLAGS   += -fPIC
+#CFLAGS  += -flto
+#CFLAGS  += -Wunsafe-loop-optimizations
+CFLAGS   += $(COVFLAGS)
 
-LDFLAGS += $(STATIC) -g $(OPT) -lm $(COVFLAGS)
-LDFLAGS += -fPIC
+LDFLAGS  += $(STATIC) -g $(OPT) -lm $(COVFLAGS)
+LDFLAGS  += -fPIC
 #LDFLAGS += -flto
 
-LCOV ?= true #lcov
-GENHTML ?= genhtml
+LCOV     ?= true #lcov
+GENHTML  ?= genhtml
 
-AR ?= ar
-RANLIB ?= ranlib
+AR       ?= ar
+RANLIB   ?= ranlib
 
-#AR = gcc-ar
-#RANLIB = gcc-ranlib
+#AR       = gcc-ar
+#RANLIB   = gcc-ranlib
 
-CC ?= cc
-#CC := /usr/local/musl/bin/musl-gcc
+#CC      ?= cc
+#CC       = /usr/local/musl/bin/musl-gcc
